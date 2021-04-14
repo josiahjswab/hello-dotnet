a39edfa191d82bedb44ea48c4f740c64ec8e1cf2
Josiah Swab <josiahjswab@gmail.com>
2021-04-14 10:46:46 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_dot_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
